d78481ea8d2b2a0752276ac87d1c131a304cfe48
Pascal Young <pascal@novaoflove.com>
2019-07-22 03:47:00 -0700

    added status command

--- scripts/initdb.js
+++ scripts/initdb.js
@@ -2,7 +2,7 @@ var sqlite = require('sqlite3')
 var db = new sqlite.Database('tc.db')
 
 db.run("CREATE TABLE reasons (reason TEXT, point_value INTEGER)")
-db.run("CREATE TABLE logs (time TEXT, username TEXT, user_id TEXT, staff_username TEXT, staff_id TEXT, reason TEXT, log_message_id TEXT, action TEXT, user_log_num)")
+db.run("CREATE TABLE logs (id INTEGER, time TEXT, username TEXT, user_id TEXT, staff_username TEXT, staff_id TEXT, reason TEXT, log_message_id TEXT, action TEXT, user_log_num)")
 db.run("CREATE TABLE evidence (id INTEGER, time TEXT, evidence_url TEXT)")
 db.run("CREATE TABLE comments (log_id INTEGER, time TEXT, staff TEXT, staff_id TEXT, content TEXT)")
 db.run("CREATE TABLE users (id TEXT PRIMARY KEY, username TEXT, logs INTEGER, points INTEGER, warnings INTEGER, kicks INTEGER, bans INTEGER)")
--- src/commands/moderation/status.js
+++ src/commands/moderation/status.js
@@ -1,6 +1,7 @@
 var { Command } = require('discord.js-commando')
 var { RichEmbed } = require('discord.js')
-
+var Logger = require('../../utils/Logger.js')
+var Moderation = require('../../structures/Moderation')
 module.exports = class StatusCommand extends Command {
     constructor(client) {
         super(client, {
@@ -18,7 +19,7 @@ module.exports = class StatusCommand extends Command {
             args: [
                 {
                     key: 'member',
-                    prompt: 'Who would you like to ban?\n',
+                    prompt: 'What member\'s status would you like to view?\n',
                     type: 'member'
                 }
             ]
@@ -34,6 +35,37 @@ module.exports = class StatusCommand extends Command {
             return msg.reply('invalid user!')
         }
 
-        return msg.reply('incomplete')
+        let warnings = await Moderation.getWarnings(member.user.id)
+        let kicks = await Moderation.getKicks(member.user.id)
+        let bans = await Moderation.getBans(member.user.id)
+        let totalLogs = await Moderation.getTotalUserLogAmount(member.user.id)
+        let points = await Moderation.getPoints(member.user.id)
+        let logs = await Moderation.getUserLogs(member.user.id)
+
+        let embed = new RichEmbed
+        embed.setColor('RANDOM')
+        embed.setTitle(`Status of ${member.user.tag} (${member.user.id})`)
+        embed.setDescription(`Use !evidence <member> to see all evidence of logs.`)
+        embed.addField(`User Stats`, `
+        Warns: **${warnings}**
+        Kicks: **${kicks}**
+        Bans: **${bans}**
+        Logs: **${totalLogs}**
+        Points: **${points}**
+        `)
+
+        // iterate through logs and add fields..
+        logs.forEach((log) => {
+            embed.addBlankField()
+            embed.addField(`User Log #${log['user_log_num']} (ID: ${log['id']})`, `
+            Reason: **${log['reason']}**
+            Action: **${log['action']}**
+            Staff: **${log['staff_username']} ${log['staff_id']}**
+            Time: **${log['time']}**
+            `)
+        })
+
+        return msg.channel.send(embed)
+            .catch(err => { Logger.error(err) })
     }
 }
