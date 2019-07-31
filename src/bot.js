c336c8e1cffebe70a694bd0280a37e77323fd676
Pascal Young <pascal@novaoflove.com>
2019-07-31 09:04:06 -0700

    added better help command. general cleanup.

--- src/bot.js
+++ src/bot.js
@@ -64,7 +64,8 @@ client.registry
         ['dev', 'Developer'],
         ['moderation', 'Moderation']
     ])
-    .registerDefaults()
+    .registerDefaultTypes()
+    .registerDefaultGroups()
     .registerTypesIn(path.join(__dirname, 'types'))
     .registerCommandsIn(path.join(__dirname, 'commands'))
 
---
+++ src/commands/general/help.js
@@ -0,0 +1,58 @@
+var { Command } = require('discord.js-commando')
+var { stripIndents } = require('common-tags')
+var { RichEmbed } = require('discord.js')
+
+module.exports = class HelpCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'help',
+            aliases: ['help', 'commands', 'cmds'],
+            group: 'general',
+            memberName: 'help',
+            description: 'Responds with list of commands',
+            guildOnly: false
+        })
+    }
+
+    hasPermission(msg) {
+        return msg.member.hasPermission('MANAGE_MESSAGES')
+    }
+
+    async run(msg) {
+        let embed = new RichEmbed
+        embed.setColor('RANDOM')
+        embed.setAuthor('Command List')
+        embed.setFooter(`Created by atom#0001 for the True Colors Administration`)
+
+        embed.setDescription(stripIndents`
+        **add**: Attaches evidence to a log.
+        Usage: !add <logId> <attachment>
+
+        **comment**: Adds a staff comment to a log.
+        Usage: !comment <logId> <message>
+
+        **convert**: Converts UTC time to any timezone.
+        Usage: !convert <time> <desired timezone>
+
+        **details**: Lists all comments left on a user log.
+        Usage: !details <logId>
+
+        **evidence**: Lists all evidence ever uploaded to user logs.
+        Usage: !evidence <user>
+
+        **help**: Displays this message; lists all available commands.
+        Usage: !help
+
+        **logevidence**: Lists all evidence uploaded on a specific log.
+        Usage: !logevidence <logId>
+
+        **logs**: Lists all logs of a specific user.
+        Usage: !logs <user>
+
+        **warn**: Warns a user and automatically generates a log.
+        Usage: !warn <user> <reason>
+        `)
+
+        return msg.channel.send(embed)
+    }
+}
